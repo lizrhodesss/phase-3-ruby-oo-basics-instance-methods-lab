26991233bdffa5d60d68b7648386246f69dd48d6
liz <rhodes.liz@gmail.com>
2022-08-30 13:27:33 -0400

    commit tree b7065b7f1c6a7e60fa468382050e8e0988e5dfc0

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+    def bark
+        puts "Woof!"
+    end
+    
+    fido = Dog.new
+    fido.bark
+    
+    snoopy = Dog.new
+    snoopy.bark
+    
+    
+    def sit
+        puts "The Dog is sitting"
+    end
+end    
+
+roach = Dog.new
+roach.sit  
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class Person
+    def talk
+        puts "Hello World!"
+    end
+    
+    
+    def walk
+        puts "The Person is walking"
+    end
+    
+end
+
+liz = Person.new
+liz.talk
+
+binding.pry
